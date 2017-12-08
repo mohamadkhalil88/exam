292da2585bae94e2508f545809c9672596ac40cf
mohamadkhalil88 <34310426+mohamadkhalil88@users.noreply.github.com>
2017-12-08 17:08:08 +0200

    Controllers & Routing

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(["auth"])->group(function() {
+    Route::resource("domains", "DomainsController");
+    Route::resource("pages", "PagesController");
+    Route::resource("posts", "PostsController");
+});
